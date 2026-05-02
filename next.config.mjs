593b75300487f0c7cf7553b98f7da17814628525
giwwi <aleksey.voloschuk@gmail.com>
2026-05-02 22:28:27 +0200

    Serve demo at root URL

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/",
+          destination: "/trust-layer-preview.html"
+        }
+      ]
+    };
+  }
+};
 
 export default nextConfig;
